b00830bb09aa63307ac489e2217eb12cf711b349
javohir <javaturin@mail.ru>
2018-05-02 12:03:22 +0500

    profile page in progress

--- app/src/main/java/com/example/developer/instagram/MainActivity.java
+++ app/src/main/java/com/example/developer/instagram/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.developer.instagram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private boolean firstTime = true;
     private ImageView allNew;
     private ImageView search;
     private ImageView addNew;
     private ImageView event;
     private ImageView profile;
+    private int previous;
 
 
     @Override
@@ -28,33 +31,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         addNew.setOnClickListener(this);
         event.setOnClickListener(this);
         profile.setOnClickListener(this);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragmentMother, new Fragment1())
+                .commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (firstTime == false){
+            setAllGrey();
+            if(previous == 1){
+                allNew.setColorFilter(getResources().getColor(R.color.customBlack));
+            }else if(previous == 2){
+                search.setColorFilter(getResources().getColor(R.color.customBlack));
+            }else if(previous == 4){
+                event.setColorFilter(getResources().getColor(R.color.customBlack));
+            }else if(previous == 5){
+                profile.setColorFilter(getResources().getColor(R.color.customBlack));
+            }
+        }
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.allNew:{
+
+                previous = 1;
                 setAllGrey();
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentMother, new Fragment1())
+                        .commit();
                 allNew.setColorFilter(getResources().getColor(R.color.customBlack));
                 break;
             }
             case R.id.search:{
+
+                previous = 2;
                 setAllGrey();
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentMother, new Fragment2())
+                        .commit();
                 search.setColorFilter(getResources().getColor(R.color.customBlack));
                 break;
             }
             case R.id.addNew:{
+                firstTime = false;
                 setAllGrey();
+                Intent intent = new Intent(this,ActivityAdd.class);
+                startActivity(intent);
                 addNew.setColorFilter(getResources().getColor(R.color.customBlack));
                 break;
             }
             case R.id.event:{
+
+                previous = 4;
                 setAllGrey();
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentMother, new Fragment4())
+                        .commit();
                 event.setColorFilter(getResources().getColor(R.color.customBlack));
                 break;
             }
             case R.id.profile:{
+
+                previous = 5;
                 setAllGrey();
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentMother, new Fragment5())
+                        .commit();
                 profile.setColorFilter(getResources().getColor(R.color.customBlack));
                 break;
             }
